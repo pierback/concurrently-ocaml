63ecad5fff8bbd60441124916318a3ee930da83e
fabian <fabian.pieringer@icloud.com>
2023-10-11 08:58:03 +0200

    kinda works

--- bin/main.ml
+++ bin/main.ml
@@ -6,50 +6,44 @@ let commands = ref []
 let speclist = [
   ("-n", Arg.String (fun s -> word_list := String.split_on_char ',' s), "List of words separated by commas");
 ]
-
 let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " -n <word_list> <command1> <command2> ..."
 
 let split_string_by_space str =
   String.split_on_char ' ' str
 
-
-  (* Define a custom sink for capturing output *)
-module OutputSink = struct
-  type t = Buffer.t
-
-  let write t bufs =
-    List.iter (fun buf -> Buffer.add_bytes t (Cstruct.to_bytes buf)) bufs
-end
-
-(* Create a custom resource handler for the output sink *)
-let output_sink_handler = Pi.sink (module OutputSink)
-
 let execute_command tag cmd =
   Eio_main.run @@ fun env ->
     try
       let proc_mgr = Eio.Stdenv.process_mgr env in
+      let output = Eio.Process.parse_out proc_mgr Eio.Buf_read.line cmd in
 
-      (* Create a buffer to capture the output *)
-      let output_buffer = Buffer.create 1024 in
-
-      (* Create a sink using the custom output_sink_handler *)
-      let output_sink = Eio.Flow.sink output_sink_handler output_buffer in
-
-      (* Spawn the process and redirect its output to the custom sink *)
-      Eio.Process.spawn proc_mgr ~stdout:output_sink cmd;
-
-      (* Wait for the process to finish *)
-      let exit_code = Eio.Process.wait proc_mgr in
-
-      (* Print the captured output (stdout and stderr) *)
-      Printf.printf "[%s] Process exited with code: %d\n" tag exit_code;
-      Printf.printf "[%s] Output:\n%s\n" tag (Buffer.contents output_buffer);
-
-      exit exit_code
+      Printf.printf "[%s]: %s\n" tag output;
+      exit 0 
     with
       | exn ->
         Printf.printf "[%s]: \n%s\n" tag (Printexc.to_string exn);
         exit 0
+
+
+let execute_command1 tag cmd =
+  let (in_channel, out_channel, err_channel) = Unix.open_process_full cmd (Unix.environment ()) in
+  let rec read_lines channel lines =
+    try
+      let line = input_line channel in
+      read_lines channel (line :: lines)
+    with
+    | End_of_file -> lines
+  in
+  let output_lines = read_lines in_channel [] in
+  let error_lines = read_lines err_channel [] in
+  let () =
+    List.iter (fun line -> Printf.printf "[%s] %s\n" tag line) output_lines;
+    List.iter (fun line -> Printf.eprintf "[%s] Error: %s\n" tag line) error_lines;
+  in
+  let exit_code = Unix.close_process_full (in_channel, out_channel, err_channel) in
+  match exit_code with
+  | WEXITED code -> exit code
+  | _ -> exit 1 (* Exit with a non-zero code if there's an error *)
 
 let () =
   Arg.parse speclist (fun arg -> commands := !commands @ [arg]) usage_msg;
