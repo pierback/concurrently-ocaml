3e453a8b523c540a66ee0dedbcd7a9c819661387
Fabian Pieringer <fabianpieringer@Fabians-MacBook-Pro.local>
2023-10-15 12:39:23 +0200

    sis works

--- bin/main.ml
+++ bin/main.ml
@@ -11,60 +11,25 @@ let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " -n <word_list> <command1> <command2
 let split_string_by_space str =
   String.split_on_char ' ' str
 
-let pretty_print input_tag error_message =
-  let lines = String.split_on_char '\n' error_message in
-  let formatted_lines = List.map (fun line ->
-    let prefix = String.make (String.length input_tag + 4) ' ' in
-    Printf.sprintf "%s%s" prefix line
-  ) lines in
-  String.concat "\n" formatted_lines
-
-let execute_command tag cmd =
-  
-  Eio_main.run @@ fun env ->
-    
-    try
-      let proc_mgr = Eio.Stdenv.process_mgr env in
-
-      let output = Eio.Switch.run @@ fun sw ->
-        let r, w = Eio.Process.pipe proc_mgr ~sw in
-        try
-          let child = Eio.Process.spawn ~sw proc_mgr ~stderr:w cmd
-          in
-          (* let child = Eio.Process.spawn ~sw cmd ~stdout:w ~stderr:w in *)
-          Eio.Flow.close w;
-          let output = Eio.Buf_read.parse_exn Eio.Buf_read.take_all r ~max_size:max_int in
-          Eio.Flow.close r;
-          Eio.Process.await_exn child;
-          
-          pretty_print tag output
-        with Eio.Exn.Io _ as ex ->
-          let error_message = (Printexc.to_string ex) in
-          pretty_print tag error_message
-        in
-
-      (* let output = Eio.Process.parse_out proc_mgr Eio.Buf_read.take_all cmd in *)
-
-        (* 
-        | `Exited code when is_success code -> ()
-        | status ->
-          let exn = Eio.Process.err (Eio.Process.Child_error status) in
-          Printf.printf "[%s]: \n%s\n" tag (Printexc.to_string exn);
-          exit 0 
-        *)
-        Printf.printf "suc[%s]: %s\n" tag output;
-
-        exit 0
-    with
-      | exn ->
-        Printf.printf "bad \n";
-        let asdf = (Printexc.to_string exn) in
-        let output = pretty_print tag asdf in
-        Printf.printf "[%s]: %s\n" tag output;
-        exit 0
+let pretty_print input_tag lines =
+  match lines with
+  | [] -> ""
+  | first_line :: rest_lines ->
+    let first_formatted_line = first_line in
+    let formatted_lines = List.map (fun line ->
+      let prefix = String.make (String.length input_tag + 4) ' ' in
+      Printf.sprintf "%s%s" prefix line
+    ) rest_lines in
+    String.concat "\n" (first_formatted_line :: formatted_lines)
 
 
-(* let execute_command1 tag cmd =
+let print_out format_out error_out tag = 
+  if String.length format_out > 0 then
+    Printf.printf "[%s]: %s\n" tag format_out 
+  else
+    Printf.printf "[%s]: %s\n" tag error_out 
+
+let execute_command1 tag cmd =
   let (in_channel, out_channel, err_channel) = Unix.open_process_full cmd (Unix.environment ()) in
   let rec read_lines channel lines =
     try
@@ -73,16 +38,19 @@ let execute_command tag cmd =
     with
     | End_of_file -> lines
   in
+  
   let output_lines = read_lines in_channel [] in
   let error_lines = read_lines err_channel [] in
-  let () =
-    List.iter (fun line -> Printf.printf "[%s] %s\n" tag line) output_lines;
-    List.iter (fun line -> Printf.eprintf "[%s] Error: %s\n" tag line) error_lines;
-  in
+
+  let format_out = pretty_print tag output_lines in
+  let error_out = pretty_print tag error_lines in
+
+  print_out format_out error_out tag;
+  
   let exit_code = Unix.close_process_full (in_channel, out_channel, err_channel) in
   match exit_code with
   | WEXITED code -> exit code
-  | _ -> exit 1 Exit with a non-zero code if there's an error *)
+  | _ -> exit 1
 
 let () =
   Arg.parse speclist (fun arg -> commands := !commands @ [arg]) usage_msg;
@@ -95,8 +63,8 @@ let () =
     | word :: rest_words, cmd :: rest_cmds ->
       let pid = Unix.fork () in
       if pid = 0 then (* Child process *)
-        let cmd_list = split_string_by_space cmd in
-        let exit_code = execute_command word cmd_list in
+        (* let cmd_list = split_string_by_space cmd in *)
+        let exit_code = execute_command1 word cmd in
         exit exit_code
       else (* Parent process *)
         process_words_and_commands rest_words rest_cmds;
