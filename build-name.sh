25516cd7dc3c6f31f10a34f642e7ec8541c572dd
fabian <fabian.pieringer@icloud.com>
2023-10-18 22:54:09 +0200

    more features

--- build-name.sh
+++ build-name.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+script_dir="$(dirname "$(readlink -f "$0")")"
+
 # Determine the platform
 if [[ "$(uname -s)" == "Linux" ]]; then
     if [[ "$(uname -m)" == "x86_64" ]]; then
-        mkdir -p ./exec/linux-x86_64/
-        echo ./exec/linux-x86_64/concurrently-ml
+        mkdir -p "${script_dir}/exec/linux-x86_64"
+        echo "${script_dir}/exec/linux-x86_64/exec/linux-x86_64/concurrently-ml"
     else
-        mkdir -p ./exec/linux-arm/
-        echo ./exec/linux-arm/concurrently-ml
+        mkdir -p "${script_dir}/exec/linux-x86_64/exec/linux-arm/"
+        echo "${script_dir}/exec/linux-x86_64/exec/linux-arm/concurrently-ml"
     fi
 elif [[ "$(uname -s)" == "Darwin" ]]; then
     if [[ "$(uname -m)" == "x86_64" ]]; then
-        mkdir -p ./exec/macos-x86_64/
-        echo ./exec/macos-x86_64/concurrently-ml
+        mkdir -p "${script_dir}/exec/linux-x86_64/exec/macos-x86_64/"
+        echo "${script_dir}/exec/linux-x86_64/exec/macos-x86_64/concurrently-ml"
     else
-        mkdir -p ./exec/macos-arm/
-        echo ./exec/macos-arm/concurrently-ml
+        mkdir -p "${script_dir}/exec/linux-x86_64/exec/macos-arm/"
+        echo "${script_dir}/exec/linux-x86_64/exec/macos-arm/concurrently-ml"
     fi
 else
     echo "Unsupported platform"
