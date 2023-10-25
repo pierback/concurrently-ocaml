b7d4ee626a9fe3ffb2f2f11725a03693fc0f7ac2
fabian <fabian.pieringer@icloud.com>
2023-10-25 08:51:25 +0200

    a few things nicely

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,14 @@
 open Unix
+open ANSITerminal
 
 let word_list = ref []
 let commands = ref []
 let spacious_mode = ref false
-
 let benchmark_mode = ref false
+let kill_others = ref false
+let kill_others_on_fail = ref false
+let kill_signal = ref "SIGTERM"
+
 
 let speclist = [
   ("-n", Arg.String (fun s -> word_list := String.split_on_char ',' s), "List of words separated by commas");
@@ -12,6 +16,9 @@ let speclist = [
   ("-sp", Arg.Unit (fun () -> spacious_mode := true), "Enable spacious mode (adds new lines)");
   ("-b", Arg.Set benchmark_mode, "Enable benchmark mode (tracks execution time)");
   ("--benchmark", Arg.Set benchmark_mode, "Enable benchmark mode (tracks execution time)");
+  ("-k", Arg.Set kill_others, "Kill other processes if one exits or dies");
+  ("--kill-others-on-fail", Arg.Set kill_others_on_fail, "Kill other processes if one exits with a non-zero status code");
+  ("--kill-signal", Arg.String (fun s -> kill_signal := s), "Signal to send to other processes if one exits or dies (default: SIGTERM)");
 ]
 
 let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " -n <word_list> <command1> <command2> ..."
@@ -21,23 +28,77 @@ let emojis = [" 🍕"; " 🌞"; " 🚗"; " 🌈"; " 🐱"; " 🌸"; " 🎈"; " �
 let split_string_by_space str =
   String.split_on_char ' ' str
 
-let pretty_print input_tag lines =
-  match lines with
-  | [] -> "" 
-  | first_line :: rest_lines ->
-    let first_formatted_line = first_line in
-    let formatted_lines = List.map (fun line ->
-      let prefix = String.make (String.length input_tag + 4) ' ' in
-      Printf.sprintf "%s%s" prefix line
-    ) rest_lines in
-    String.concat "\n" (first_formatted_line :: formatted_lines)
+let colorize str color =
+  sprintf [ANSITerminal.Foreground color] str
 
-let print_out format_out error_out tag =
-  let newline = if !spacious_mode then "\n" else "" in
+
+let unified_pretty_print ?(format_out=[]) ?(error_out=[]) ?(tag="") ?(color="default") lines =
+  let prefix_char = colorize "| " color in
+  let newline = if !spacious_mode || !benchmark_mode || List.length format_out > 0 then "\n" else "" in
+
+  match lines with
+  | [] -> ""
+  | first_line :: rest_lines ->
+    let first_formatted_line =
+      if !spacious_mode || !benchmark_mode then
+        Printf.sprintf "%s%s" prefix_char first_line
+      else
+        first_line
+    in
+
+    let formatted_lines =
+      if !spacious_mode || !benchmark_mode || List.length rest_lines > 0 then
+        List.map (fun line -> Printf.sprintf "%s%s" prefix_char line) rest_lines
+      else
+        List.map (fun line -> line) rest_lines
+    in
+
+    let first_line_prefix =
+      if !spacious_mode || !benchmark_mode || List.length rest_lines > 0 then "\n" else ""
+    in
+
+    let formatted_output = String.concat first_line_prefix (first_formatted_line :: formatted_lines) in
+
+    if List.length format_out > 0 then
+      Printf.sprintf "%s%s%s:%s%s\n" newline prefix_char tag newline (String.concat newline format_out)
+    else if List.length error_out > 0 then
+      Printf.sprintf "%s%s%s:%s%s\n" newline prefix_char tag newline (String.concat newline error_out)
+    else
+      Printf.sprintf "%s" formatted_output
+  
+  
+
+let pretty_print _ lines color =
+  let prefix_char = colorize "| " color in
+  match lines with
+  | [] -> ""
+  | first_line :: rest_lines ->
+    let first_formatted_line = if !spacious_mode || !benchmark_mode  then
+      if List.length rest_lines > 0 then 
+        Printf.sprintf "%s%s" prefix_char first_line
+      else
+        Printf.sprintf "%s%s" prefix_char first_line
+    else
+      first_line
+    in
+    let formatted_lines =
+      if !spacious_mode || !benchmark_mode || List.length rest_lines > 0 then
+        List.map (fun line -> Printf.sprintf "%s%s" prefix_char line) rest_lines
+      else
+        List.map (fun line -> line) rest_lines
+    in
+    let first_line_prefix = if !spacious_mode || !benchmark_mode || List.length rest_lines > 0 then "\n" else "" in
+    String.concat first_line_prefix (first_formatted_line :: formatted_lines)
+  
+  
+
+let print_out format_out error_out tag color =
+  let prefix_char = colorize "| " color in
+  let newline = if !spacious_mode || !benchmark_mode || String.length format_out > 0 then "\n" else "" in
   if String.length format_out > 0 then
-    Printf.printf "%s[%s]: %s\n" newline tag format_out
+    Printf.printf "%s%s%s:%s%s\n" newline prefix_char tag newline format_out
   else
-    Printf.printf "%s[%s]: %s\n" newline tag error_out
+    Printf.printf "%s%s%s:%s%s\n" newline prefix_char tag newline error_out
 
 let format_time elapsed_time =
   if elapsed_time >= 1.0 then
@@ -56,12 +117,23 @@ let format_time elapsed_time =
     in
     Printf.sprintf "%d,%02d ms" milliseconds rounded_nanoseconds
   
+
+let format_to_grey str =
+  sprintf [ANSITerminal.Foreground ANSITerminal.Black] "%s" str
+
+let get_color error_lines =
+  let has_error = not (List.length error_lines = 0) in
+  if has_error then
+    ANSITerminal.Red
+  else
+    ANSITerminal.Green
+    
 let generate_tag tag elapsed_time =
   if !benchmark_mode then
     let time_str = format_time elapsed_time in
-    Printf.sprintf "tag: %s | time: %s" tag time_str
+    Printf.sprintf "[%s] %s" tag (format_to_grey time_str) 
   else
-    Printf.sprintf "%s" tag
+    Printf.sprintf "[%s]" tag
 
 let execute_command tag cmd =
   let start_time = Unix.gettimeofday () in
@@ -77,15 +149,23 @@ let execute_command tag cmd =
   let output_lines = read_lines in_channel [] in
   let error_lines = read_lines err_channel [] in
 
-  let format_out = pretty_print tag output_lines in
-  let error_out = pretty_print tag error_lines in
-
   let end_time = Unix.gettimeofday () in
   let elapsed_time = end_time -. start_time in
 
   let new_tag = generate_tag tag elapsed_time in
 
-  print_out format_out error_out new_tag;
+  let time_str = format_time elapsed_time in
+
+
+  let format_out = pretty_print time_str output_lines ANSITerminal.Green in
+  let error_out = pretty_print time_str error_lines ANSITerminal.Red in
+
+  let color = get_color error_lines in
+
+  let formatted_output = unified_pretty_print ~format_out:"Additional Info" ~tag:tag lines color in
+
+  (* If you want to print it to the console, you can do so like this: *)
+  Printf.printf "%s" formatted_output;
 
   let exit_code = Unix.close_process_full (in_channel, out_channel, err_channel) in
   match exit_code with
@@ -96,8 +176,6 @@ let select_random_emoji () =
   let index = Random.int (List.length emojis) in
   List.nth emojis index
 
-
-
 let generate_word_list count =
   let rec generate_words n acc =
     if n <= 0 then acc
@@ -106,39 +184,65 @@ let generate_word_list count =
   in
   generate_words count []
 
+  let create_child_process word cmd =
+    let pid = Unix.fork () in
+    if pid = 0 then (* Child process *)
+      let exit_code = execute_command word cmd in
+      exit exit_code
+    else (* Parent process *)
+      pid
+  
+let rec execute_commands_and_track_failures words cmds failures successes =
+  match words, cmds with
+  | [], [] -> (failures, successes)
+  | _, [] | [], _ -> (failures, successes)
+  | word :: rest_words, cmd :: rest_cmds ->
+    let pid = create_child_process word cmd in
+    match Unix.waitpid [] pid with
+    | _, WEXITED 1 -> execute_commands_and_track_failures rest_words rest_cmds (failures + 1) successes
+    | _, WEXITED _ -> execute_commands_and_track_failures rest_words rest_cmds failures (successes + 1)
+    | _, WSIGNALED _ | _, WSTOPPED _ -> execute_commands_and_track_failures rest_words rest_cmds (failures + 1) successes
+
+let handle_kill_signal signal pids =
+List.iter (fun pid -> Unix.kill signal pid) pids
+
+let main () =
+Arg.parse speclist (fun arg -> commands := !commands @ [arg]) usage_msg;
+
+let word_list_length = List.length !word_list in
+
+if word_list_length > 0 && word_list_length <> List.length !commands then
+  begin
+    Printf.printf "Error: Number of words and commands must match.\n";
+    exit 1
+  end;
+
+if word_list_length = 0 then
+  begin
+    Random.self_init ();  (* Seed the random number generator *)
+    word_list := generate_word_list (List.length !commands);
+  end;
+
+let child_processes = ref [] in
+let (failures, _) = execute_commands_and_track_failures !word_list !commands 0 0 in
+
+(* if failures > 0  then
+  Printf.printf "Some commands failed.\n"
+else
+  Printf.printf "All commands succeeded.\n"; *)
+
+if !kill_others || !kill_others_on_fail then
+  begin
+    let sig_to_send =
+      match !kill_signal with
+      | "SIGTERM" -> 15
+      | "SIGKILL" -> 9
+      | _ -> failwith "Invalid signal"
+    in
+    handle_kill_signal sig_to_send !child_processes;
+  end;
+
+exit (if failures > 0 then 1 else 0)
+
 let () =
-  Arg.parse speclist (fun arg -> commands := !commands @ [arg]) usage_msg;
-
-  let word_list_length = List.length !word_list in
-
-  if word_list_length > 0 && word_list_length <> List.length !commands then
-    begin
-      Printf.printf "Error: Number of words and commands must match.\n";
-      exit 1
-    end;
-
-  if word_list_length = 0 then
-    begin
-      Random.self_init ();  (* Seed the random number generator *)
-      word_list := generate_word_list (List.length !commands);
-    end;
-
-  let rec process_words_and_commands words cmds =
-    match words, cmds with
-    | [], [] -> ()
-    | _, [] | [], _ -> ()
-    | word :: rest_words, cmd :: rest_cmds ->
-      let pid = Unix.fork () in
-      if pid = 0 then (* Child process *)
-        let exit_code = execute_command word cmd in
-        exit exit_code
-      else (* Parent process *)
-        process_words_and_commands rest_words rest_cmds;
-        let (_, status) = Unix.waitpid [] pid in
-      match status with
-      | WEXITED _ -> ()
-      | WSIGNALED _ -> ()
-      | WSTOPPED _ -> ()
-  in
-
-  process_words_and_commands !word_list !commands
+  main ()
