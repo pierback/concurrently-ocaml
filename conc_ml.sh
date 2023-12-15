167e55621074a3a12d29274b53a2d0be1cc91b46
Fabian Pieringer <fabian.pieringer@enpal.de>
2023-12-15 20:42:08 +0100

    0.0.13

--- conc_ml.sh
+++ conc_ml.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 script_dir="$(dirname "$(readlink -f "$0")")"
-build_name_script="${script_dir}/build-name.sh"
+build_name_script_path="${script_dir}/build-name.sh"
 
-$($build_name_script) "$@"
+path=$($build_name_script_path)
+
+cp -L "${path}" "${script_dir}/"
