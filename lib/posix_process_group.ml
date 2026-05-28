a48f9ab08d4ea69e383eef114c14429f6a98dfb3
Fabian Pieringer <f.pieringer@celonis.com>
2026-05-28 12:44:36 +0200

    Own POSIX process lifecycle

--- lib/posix_process_group.ml
+++ lib/posix_process_group.ml
@@ -1,15 +1,5 @@
 [@@@alert "-unstable"]
 
-module Fork_action = Eio_unix.Private.Fork_action
-
-external action_setsid : unit -> Fork_action.fork_fn
-  = "concurrently_fork_action_setsid"
-
-let action_setsid = action_setsid ()
-
-let start_new_session =
-  { Fork_action.run = (fun k -> k (Obj.repr (action_setsid, ()))) }
-
 let signal_group ~pid signal =
   try
     Unix.kill (-pid) signal;
--- lib/posix_process_group.mli
+++ lib/posix_process_group.mli
@@ -1,3 +1,1 @@
-val start_new_session : Eio_unix.Private.Fork_action.t
-
 val signal_group : pid:int -> int -> (bool, string) result
